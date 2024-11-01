231b11bf865a4262ea83713201b6eebfd4c17832
Unni Krishnan <ambady1960@hotmail.com>
2024-11-01 20:19:15 +0800

    Update enquiry.py

--- enquiry.py
+++ enquiry.py
@@ -5,8 +5,4 @@ def enquiry():
     st.write("This is the Enquiry section.")
     # Add any additional functionality needed for the Enquiry section here.
     
-    # Go Home button at the bottom left
-    st.markdown('<div style="position: absolute; bottom: 0; left: 0;">', unsafe_allow_html=True)
-    if st.button("Go Home"):
-        go_home()
-    st.markdown('</div>', unsafe_allow_html=True)
+   
