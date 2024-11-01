7a5008d7dc36668a6b8a30731a10c648f162b6f3
Unni Krishnan <ambady1960@hotmail.com>
2024-11-01 20:18:45 +0800

    Update ack.py

--- ack.py
+++ ack.py
@@ -5,8 +5,4 @@ def ack():
     st.write("This is the Acknowledgement (Ack) section.")
     # Add any additional functionality needed for the Ack section here.
     
-    # Go Home button at the bottom left
-    st.markdown('<div style="position: absolute; bottom: 0; left: 0;">', unsafe_allow_html=True)
-    if st.button("Go Home"):
-        go_home()
-    st.markdown('</div>', unsafe_allow_html=True)
+   
